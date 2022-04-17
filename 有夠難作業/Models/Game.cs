99b1d35e51b03c99fdcd5b687a3264771c3c0203
timlan0214 <timlan8888@gmail.com>
2022-04-17 18:32:49 +0800

    好難

---
+++ 有夠難作業/Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace 有夠難作業.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public int Lovel{ get; set; }
+        public string Name { get; set; }
+        public int Atk { get; set; }
+        public int Hp { get; set; }
+    }
+}
